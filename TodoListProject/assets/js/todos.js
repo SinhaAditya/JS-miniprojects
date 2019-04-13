6ceedf004e3cb2975b90695b41902613e2f8d2d3
Aditya  Sinha <sinha.aditya.18@gmail.com>
2019-04-13 10:36:16 +1000

    initial commit - colorgame and todolist

---
+++ TodoListProject/assets/js/todos.js
@@ -0,0 +1,26 @@
+// mark todo as done
+
+$("ul").on("click", "li", function() {
+	$(this).toggleClass("completed");
+});
+
+// delete todo
+
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(300, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fas fa-trash'></i></span> "+todoText+"</li");
+	}
+});
+
+$(".fa-plus-circle").click(function() {
+	$("input[type='text']").fadeToggle();
+});
